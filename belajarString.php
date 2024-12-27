199efb3349e446576cb5273137ad2606382efa38
Sunarto Utina <nartosunarto73126@gmail.com>
2024-12-27 20:00:27 +0700

    belajar string single quote dan double quote

---
+++ belajarString.php
@@ -0,0 +1,17 @@
+<?php
+$string1 = 'Ini adalah string';
+$string2 = 'Buku ini berjudul \'Belajar pemrograman Framework Laravel\' ';
+$string3 = 'Kalimat ini merupakan 
+satus baris yang sama';
+$string4 = 'kita simpan file di c:\\laragon\\www\\belajar-laravel';
+$string5 = 'Buku ini istimewa. Karena ada pembahasan untuk case Digital Startup';
+$hewan = 'Kuda Nill';
+$test = "Menjadi \nManusia dan $hewan";
+
+echo $string1 . '<br>';
+echo $string2 . '<br>';
+echo $string3 . '<br>';
+echo $string4 . '<br>';
+echo $string5 . '<br>';
+echo $test . '<br>';
+?>
